281970cbd0fab2169d2c51eaf0d1482d4bb4d47a
TimeYurii <chasovshchykov.yurii@comp-sc.if.ua>
2025-03-11 07:43:16 +0200

    1.2.3-1.2.5.js

---
+++ lab_2/1.2.3-1.2.5.js
@@ -0,0 +1,20 @@
+const car1 = new Object();
+
+car1.color = "Green";
+car1.maxSpeed = 300;
+car1.driver = {
+    name: "Bondar Kostiantyn", 
+    category: "C",
+    personalLimitations: "No driving at night"
+};
+car1.tuning = true; 
+car1.numberOfAccidents = 0; 
+
+car1.drive = () => {
+    console.log("I am not driving at night");
+};
+
+console.log("Task 1.2.3");
+console.log("car1:", car1);
+car1.drive();
+console.log("----------------------------------");
